7759ccdae94014c0bb76b5c5458206691e1189c8
BoredLudleth <mozgovoy.ka@gmail.com>
2023-03-26 21:57:04 +0300

    Added %s, %x and maybe smth more. Tests complete

--- printf.asm
+++ printf.asm
@@ -3,24 +3,38 @@ BITS 64                                                     ; chosen 64-bits reg
 global _start
 
 section .data
-    msg db "%c - char %b - bin", 0xA, "Hello darkness my old friend%%", 0xA, 0           ; phrase to print
-    var2 db 'U'
-    var1 dq 58
+    msg db  "%c - char", 0xA, "%s - string", 0xA, "%x - hex", 0xA, "%o - oct", 0xA, "%b - bin", 0xA, "Hello darkness my old friend%%", 0xA, 0           ; phrase to print
+    var1 db 'v'
+    var2 db "1488", 0
+    var3 dq 58
+    var4 dq 58
+    var5 dq 58
 
 section .text
     %include "printf.inc"                                   ; add body of printf
     %include "printsym.inc"
     %include "printspecial.inc"
     %include "binary.inc"
-    %include "printchar.inc"
+    %include "char.inc"
+    %include "oct.inc"
+    %include "hex.inc"
+    %include "string.inc"
 
     _start:
 
-        mov rbx, [var1]
+        mov rbx, var5
         push rbx
 
-        xor bx,  bx
+        mov rbx, var4
+        push rbx
+
+        mov rbx, var3
+        push rbx
+
         mov rbx, var2
+        push rbx
+
+        mov rbx, var1
         push rbx
 
         mov rax, msg
